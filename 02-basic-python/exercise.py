811f6fb3bdea2b0d436154cb3c64c34f285a6286
Luis Garcia <luis.garcia@utah.edu>
2020-01-08 20:55:22 -0700

    Added and altered a few things

--- 02-basic-python/exercise.py
+++ 02-basic-python/exercise.py
@@ -3,4 +3,5 @@ def add_numbers(a, b):
 
 print(add_numbers(3, 7))
 print(add_numbers(14.22, 19))
-print(a)
+print(a) # Reason why this specific line doesnt work because
+         # python assumes 'a' is a variable
